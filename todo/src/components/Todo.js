71d2f859561b383a9a6f1e79f7673766f4139a1c
Paul Won Lee <PaulLee@Pauls-MBP.lan1>
2020-04-22 15:27:37 -0700

    added useState and useReducer to Todo.js

--- todo/src/components/Todo.js
+++ todo/src/components/Todo.js
@@ -2,6 +2,9 @@ import React, {useState, useReducer} from 'react';
 import {initialState, todoReducer} from '../reducers/todoReducer';
 
 const Todo = () => {
+    const [newTodo, setNewTodo] = useState("");
+    const [state, dispatch] = useReducer(todoReducer, initialState);
+
     return(
         <div>
             <div>
